c1e41b91fbed228debef45f35052cb01859e105a
mitchsmii <143842296+mitchsmii@users.noreply.github.com>
2025-11-21 15:01:57 -0700

    changes for deployment to Elastic Beanstalk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -59,4 +60,5 @@ app.post('/edithabit', (req, res) => {
 
 
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+
+app.listen(PORT, () => console.log("Server running"));
