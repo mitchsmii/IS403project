e12e6d2b47cb227e28a80ebb25d7c67cf25ba9e2
mitchsmii <143842296+mitchsmii@users.noreply.github.com>
2025-11-22 18:01:26 -0700

    working version of app

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d http://mitchs.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email mitch.smi45@gmail.com
